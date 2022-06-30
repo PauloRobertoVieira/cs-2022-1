7a45ad56e90a2bc2924c7c5c2970658bc6e6ab17
PauloVieira <paulovieira@discente.ufg.br>
2022-06-29 22:53:59 -0300

    Calcular idade em dias

---
+++ tarefa006/idade.py
@@ -0,0 +1,5 @@
+ano = int(input('Informe sua idade '))
+mes = int(input('Informe mes nascimento '))
+dia = int(input('Informe dia nascimento '))
+idade = (ano*365)+(mes*30)+dia
+print("%d dias" % idade)
