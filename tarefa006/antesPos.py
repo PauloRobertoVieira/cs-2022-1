56c64b720cefb4799c8bde2ba2439c7a3d67748a
PauloVieira <paulovieira@discente.ufg.br>
2022-06-29 22:58:19 -0300

    Verifica Antecessor e Sucessor de um numero

---
+++ tarefa006/antesPos.py
@@ -0,0 +1,6 @@
+numero = int(input('Informe um numero: '))
+antecessor = numero - 1
+print('O antecessor de %d é %d' % (numero, antecessor))
+
+sucessor = numero + 1
+print('O sucessor de %d é %d' % (numero, sucessor))
