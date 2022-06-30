0eb00f8fc918ac44d1dd379c613a919bb7a6fe30
PauloVieira <paulovieira@discente.ufg.br>
2022-06-29 22:54:39 -0300

    Calcular médias

---
+++ tarefa006/media.py
@@ -0,0 +1,11 @@
+primeiraMedia = (8+9+7)/3
+print('Primeira média é %.0f' % primeiraMedia)
+
+segundaMedia = (4+5+6)/3
+print('Segunda média é %.0f' % segundaMedia)
+
+somaMedias = primeiraMedia+segundaMedia
+print('Soma das médias é %.1f' % somaMedias)
+
+mediaMedias = somaMedias/2
+print('A média das médias é %.1f' % mediaMedias)
